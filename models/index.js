fff8c2604660cdd6da5366d23d9c41b010802bc0
marlowe <marlowepdx@gmail.com>
2022-07-27 16:28:31 -0700

    post routes created

--- models/index.js
+++ models/index.js
@@ -2,5 +2,15 @@
 
 const User = require('./User');
 const Post = require('./Post');
+const { unsubscribe } = require('../routes');
+
+// create associations here (like the constraints)
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+//inverse association here
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 module.exports = { User };
